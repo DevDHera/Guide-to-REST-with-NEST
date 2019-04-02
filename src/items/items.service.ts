a5e601caca7f0970d1074dc09d1840f25d5867fd
Devin Herath <devdhera@gmail.com>
2019-04-02 08:55:32 +0530

    Added findAll service

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    private readonly items: Item[] = [
+        {
+            id: '3434343434',
+            name: 'Item One',
+            qty: 100,
+            description: 'This is Item One',
+        },
+        {
+            id: '9867566456',
+            name: 'Item Two',
+            qty: 50,
+            description: 'This is Item Two',
+        },
+    ];
+}
